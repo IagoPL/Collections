ed6ffa1a8dae543989f1eb8f886a41761e67cd12
IagoPL <iagoprietolamas@gmail.com>
2023-02-01 23:46:09 +0100

    Collections

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -9,26 +9,6 @@ class CollectionController extends Controller
 {
     //
 
-    public function collection_class(){
-
-
-        //Crear una nueva colección usando la clase Colección
-        $collection1 = new Collection(['123456789w','987654321e','123789456t','456123789f','789546123a']);
-
-        //volcar el contenido variable en el navegador
-        dd($collection1);
-    }
-
-    public function collect_method(){
-
-        //Create a new collection using the collect method
-        $collection2 = collect(["Tomas", "Better", "Raquel", "Maria", "Raul"]);
-
-        //dump the variable content in the browser
-        dd($collection2);
-
-    }
-
     public function search_data()
     {
         //Declarar una colección
